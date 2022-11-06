0043330d0c876dda66c36faf3cb9b0b06d8bf1d2
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:03 +0100

    Treat VSCodium like IntelliJ

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -648,7 +648,7 @@ awful.rules.rules = {
 client.connect_signal("manage", function(c)
     -- Set the windows at the slave,
     -- i.e. put it at the end of others instead of setting it master.
-    if not awesome.startup and c.class ~= "jetbrains-idea-ce" then awful.client.setslave(c) end
+    if not awesome.startup and c.class ~= "jetbrains-idea-ce" and c.class ~= "VSCodium" then awful.client.setslave(c) end
 
     c.size_hints_honor = false
 
