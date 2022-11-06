01e48a68c74ce5e4e3f23648f5a27212c0fa08ac
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:04 +0100

    Fix SRC/rtgg panel elements

--- .config/polybar/rtgg.sh
+++ .config/polybar/rtgg.sh
@@ -4,8 +4,8 @@ URL='https://racetime.gg/oot/races/data'
 
 JSON=$(curl -s $URL)
 
-COUNT=$(echo $JSON | jq -s '.[0].races | map(select(.recorded == false and .recordable == true and .status.value == "finished")) | length')
+COUNT=$(echo $JSON | jq -s '.[0].races | map(select(.recorded == false and .recordable == true and .status.value == "finished")) | length' 2>/dev/null)
 
-[[ COUNT -ne 0 ]] && echo "⏱️ $COUNT"
+[[ COUNT -ne 0 ]] && echo "⏱️ $COUNT" || echo ""
 exit 0
 
