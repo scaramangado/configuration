e345b204411d43ef3b3ff9e36d9d748510286528
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:03 +0100

    Switch to picom

--- .config/awesome/apps/autostart.lua
+++ .config/awesome/apps/autostart.lua
@@ -5,10 +5,11 @@ return {
 		"jetbrains-toolbox --minimize",
 		"blueman-tray",
 		"nextcloud",
-		"keepassxc"
+		"keepassxc",
+		"/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1"
 	},
 	shell = {
-		"killall compton ; compton",
+		"killall picom ; picom",
 		"killall pasystray ; pasystray &",
 		"pgrep Discord || discord --start-minimized",
 		"while [ true ] ; do echo -n | xsel -n -i ; sleep 0.5 ; done",
