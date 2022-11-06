6089eb5d906e99f00f36ba8c148f4cbea894c56d
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:03 +0100

    Small changes

--- .config/awesome/apps/autostart.lua
+++ .config/awesome/apps/autostart.lua
@@ -8,12 +8,13 @@ return {
 	shell = {
 		"killall compton ; compton",
 		"killall pasystray ; pasystray &",
-		"pgrep Discord || flatpak run com.discordapp.Discord --start-minimized",
+		"pgrep Discord || discord --start-minimized",
 		"while [ true ] ; do echo -n | xsel -n -i ; sleep 0.5 ; done",
 		"nitrogen --random --set-scaled ~/.config/awesome/wallpapers",
 		"~/.config/polybar/launch.sh",
 		"pgrep dropbox && killall dropbox ; flatpak run com.dropbox.Client",
-		"pgrep thunderbird || thunderbird"
+		"pgrep thunderbird || thunderbird",
+		"/usr/libexec/deja-dup/deja-dup-monitor"
 	}
 }
 
