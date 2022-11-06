1c73c660efd3984e4fa4222d65fa9746d397d927
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:06 +0100

    Optimize temperature logging

--- .scripts/log_temps.sh
+++ .scripts/log_temps.sh
@@ -2,11 +2,14 @@
 
 DIR=$HOME/.temp-log
 FILE=$DIR/$(who -b | sed -rn "s/.*boot\\s+(.*)/\\1/p" | sed "s/ /_/").log
+TEMP_FILE=temp.log
 
 mkdir -p $DIR
 
 for i in {1..59}; do
-	sensors > $FILE
+	sensors > $TEMP_FILE
+	[ -f $FILE ] || touch $FILE
+	[ -s $TEMP_FILE ] && rm $FILE && mv $TEMP_FILE $FILE && free -h >> $FILE
 	sleep 1
 done
 
