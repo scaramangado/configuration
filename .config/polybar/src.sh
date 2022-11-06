28493f38d3f2c9184f05e602697be354682a7585
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:06 +0100

    Change number of runs left open

--- .config/polybar/src.sh
+++ .config/polybar/src.sh
@@ -13,6 +13,6 @@ EXT_COUNT=$(echo $EXT_JSON | jq .pagination.size 2>/dev/null)
 
 COUNT=$(( $MAIN_COUNT + $EXT_COUNT ))
 
-[[ COUNT -ne 5 ]] && echo "🏆 $COUNT" || echo ""
+[[ COUNT -ne 4 ]] && echo "🏆 $COUNT" || echo ""
 exit 0
 
