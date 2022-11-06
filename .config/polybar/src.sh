126b7e65ca09d508f1e9cc9ac74ea23c330ca034
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:06 +0100

    Fix SRC script (comments with line breaks)

--- .config/polybar/src.sh
+++ .config/polybar/src.sh
@@ -8,8 +8,8 @@ EXT_LB_URL='https://www.speedrun.com/api/v1/runs?status=new&game=76rkv4d8&max=10
 MAIN_JSON=$(curl -s $MAIN_LB_URL)
 EXT_JSON=$(curl -s $EXT_LB_URL)
 
-MAIN_COUNT=$(echo $MAIN_JSON | jq .pagination.size 2>/dev/null)
-EXT_COUNT=$(echo $EXT_JSON | jq .pagination.size 2>/dev/null)
+MAIN_COUNT=$(echo $MAIN_JSON | tr -d '\r\n' | jq .pagination.size 2>/dev/null || echo "0" )
+EXT_COUNT=$(echo $EXT_JSON | tr -d '\r\n' | jq .pagination.size 2>/dev/null || echo "0" )
 
 COUNT=$(( $MAIN_COUNT + $EXT_COUNT ))
 
