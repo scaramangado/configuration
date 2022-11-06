6c22650d7958d3359f46f943ed528fbd6b82b6cc
scaramangado <3808178+scaramangado@users.noreply.github.com>
2022-11-06 14:38:04 +0100

    Don't show untracked files by default

--- .config/init.sh
+++ .config/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo ".config-repo/" >> $HOME/.gitignore
 git clone --bare https://github.com/scaramangado/configuration.git $HOME/.config-repo
+git --git-dir=$HOME/.config-repo/ --work-tree=$HOME config --local status.showUntrackedFiles no
 [ -f .zshrc] && mv .zshrc .zshrc.bak
 git --git-dir=$HOME/.config-repo/ --work-tree=$HOME checkout
 source .zshrc
